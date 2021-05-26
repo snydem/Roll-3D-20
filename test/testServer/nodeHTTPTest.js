2b88c43eb8d3669253fb3e489c185d3c24b8dbc9
snydem <cs059588@gmail.com>
2021-05-25 21:05:12 -0400

    used npm to install threejs, as well as did some refactoring of the backend test Server

--- test/testServer/nodeHTTPTest.js
+++ test/testServer/nodeHTTPTest.js
@@ -9,6 +9,12 @@ const server = http.createServer( function(request, response) {
 	// I want to analyze a request because I'm not exactly what happens
 	// when an http request happens.
 
+
+	// Originally I was having an issue understanding the request url, but
+	// now it makes sense. Because the request url is just requesting the
+	// homepage, the only location is has to find, is the base location.
+	// its like the root node of my file system, so its just requesting
+	// the base point.
 	const {method, url} = request;
 	const {headers} = request;
 	const userAgent = headers['user-agent']
@@ -17,7 +23,7 @@ const server = http.createServer( function(request, response) {
 	console.log('request url: ' + url);
 
 	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/html');
+	response.setHeader('Content-Type', 'text/html');	
 	response.write(htmlServed);
 	response.end();
 });
